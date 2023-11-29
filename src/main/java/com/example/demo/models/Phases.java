98f24e31b61ddf642677777d5f6f0243afd839e8
Saif p <saifalli.sombare@c2info.com>
2023-11-29 10:32:48 +0530

    key name changes

--- src/main/java/com/example/demo/models/Phases.java
+++ src/main/java/com/example/demo/models/Phases.java
@@ -4,9 +4,12 @@ import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonAlias;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonValue;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -18,39 +21,60 @@ import lombok.NoArgsConstructor;
 @Builder
 public class Phases {
 	// REACTOR
+	@JsonProperty("REACTOR")
 	private List<Parameter> reactorPhase;
 
 	// INITIATION
+	@JsonProperty("INITIATION")
 	private List<Parameter> initiation;
 
 	// EMULSION RAW MATERIAL
+	@JsonProperty("EMULSION RAW MATERIAL")
+
 	private List<Parameter> emulsion;
 
 	// ONE TIME PPS
+	@JsonProperty("ONE TIME PPS")
+
 	private List<Parameter> oneTimePPS;
 
 	// PPS1
+	@JsonProperty("PPS 1")
+
 	private List<Parameter> pPS1;
 
 	// PPS2
+	@JsonProperty("PPS 2")
+
 	private List<Parameter> pPS2;
 
 	// PPS3
+	@JsonProperty("PPS 3")
+
 	private List<Parameter> pPS3;
 
 	// PPS4
+	@JsonProperty("PPS 4")
+
 	private List<Parameter> pPS4;
 
 	// CONTINUOUS PPS
+	@JsonProperty("CONTINUOUS PPS")
+
 	private List<Parameter> continuousPPS;
 
 	// QC
+	@JsonProperty("QC")
+
 	private List<Parameter> qC;
 
 	// POST ADDITION
+	@JsonProperty("POST ADDITION")
+
 	private List<Parameter> postAddition;
 
 	// TOTAL
+
 	private BigDecimal totalWeight;
 	private BigDecimal whiteWaterQuantity;
 	private BigDecimal blendingMaterial;
